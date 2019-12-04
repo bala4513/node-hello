a80240159661e634bcc0da0bd3f339bc077fbb25
Balagobinath Vellingiri <38631141+bala4513@users.noreply.github.com>
2019-12-05 02:50:54 +0530

    running from container

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from bala and kathir!!!!!!!!!!!!!!!!\n');
+  res.end('Hello World running from container!!!!!!!!!!!!!!!!\n');
 });
 
 server.listen(port, hostname, () => {
