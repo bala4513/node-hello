70ea15dd11b13261c210d6ab3a94b42a6b03f5d3
Balagobinath Vellingiri <38631141+bala4513@users.noreply.github.com>
2019-12-05 03:41:28 +0530

    6

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World LIGHT YAGAMI 5 running from container!!!!!!!!!!!!!!!!\n');
+  res.end('Hello World LIGHT YAGAMI 6 running from container!!!!!!!!!!!!!!!!\n');
 });
 
 server.listen(port, hostname, () => {
