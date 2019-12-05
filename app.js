ed1d31416571acefb5edb0434f41d44490609265
Balagobinath Vellingiri <38631141+bala4513@users.noreply.github.com>
2019-12-06 03:36:45 +0530

    updated jenkins config removed install

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World LIGHT YAGAMI 11 running from container checking swarm !!!!!!!!!!!!!!!!\n');
+  res.end('Hello World LIGHT YAGAMI 11 running from container checking swarm changed congif in jenkins !!!!!!!!!!!!!!!!\n');
 });
 
 server.listen(port, hostname, () => {
